e6f6b013293109f8399440884173f2dd9b43dbea
pavelkiru <pavelkirushchenko@gmail.com>
2024-02-19 12:12:26 +0300

    CRUD post delete with delete post image. next 21

--- app/Http/Controllers/Admin/Post/DestroyController.php
+++ app/Http/Controllers/Admin/Post/DestroyController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 
 class DestroyController extends Controller
 {
     public function __invoke( Post $post )
     {
 
-     //   dd($post);
         $post->delete();
+
+        Storage::disk('public')->delete([$post['preview_image'],$post['main_image']]);
         return redirect()->route('admin.posts.index');
 
     }
